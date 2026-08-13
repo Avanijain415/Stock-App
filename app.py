11ad21b019ce4fd36ce531e15d72abd41965cba2
Avanijain415 <avanimehta415@gmail.com>
2026-08-13 21:41:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,6 +173,8 @@ def get_activity():
     conn.close()
     return jsonify({'logs': logs})
 
-if __name__ == '__main__':
+with app.app_context():
     init_db()
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
